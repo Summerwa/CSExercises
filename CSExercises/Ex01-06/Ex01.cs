cef905ecca76b7c57b66e1fb2749ef2c5e859872
Summerwa <JWang004>
2015-08-16 14:08:22 +0800

    Completed Exercise 1-Juan

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -10,7 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+           Console.Write("Enter you name:");
+           String Name;
+           Name=Console.ReadLine();
+           Console.WriteLine("Good Morning\t"+Name);
+
         }
     }
 }
