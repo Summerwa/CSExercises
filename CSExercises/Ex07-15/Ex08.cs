e376c1e20c7acc4e67369859aa280c06a67a77b7
Summerwa <JWang004>
2015-08-16 16:08:42 +0800

    Completed Exercise 08-Juan

--- CSExercises/Ex07-15/Ex08.cs
+++ CSExercises/Ex07-15/Ex08.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Input a temperature in Centigrade scale:");
+            double C;
+            double F;
+            C= Convert.ToDouble(Console.ReadLine());
+            F =1.8*C+32;
+            Console.WriteLine("The temperature in Fahrenheit scale is:{0}",F); 
         }
     }
 }
