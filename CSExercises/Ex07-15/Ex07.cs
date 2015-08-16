d1daaf1913acb6c9408e543fff59fa5ad53197c9
Summerwa <JWang004>
2015-08-16 15:52:28 +0800

    Completed Exercise 07-Juan

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Input your salary:");
+            double salary;
+            double income;
+            salary = Convert.ToDouble(Console.ReadLine());
+            income = salary + 0.1 * salary + 0.03 * salary;
+            Console.WriteLine("The total income is:{0:0.00}", income);
         }
     }
 }
