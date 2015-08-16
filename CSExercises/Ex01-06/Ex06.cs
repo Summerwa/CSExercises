df7551d73063d6b0dd5ea15e2a2098ef6fba9ad9
Summerwa <JWang004>
2015-08-16 15:40:43 +0800

    Completed Exercise 06-Juan

--- CSExercises/Ex01-06/Ex06.cs
+++ CSExercises/Ex01-06/Ex06.cs
@@ -10,7 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Input an integer:");
+            int In;
+            double Out;
+            In = Convert.ToInt32(Console.ReadLine());
+            Out = Math.Sqrt(In);
+            Console.WriteLine("The square root is: {0}",Out);
+            Console.WriteLine("Rounded: {0:0.000}", Out);
         }
     }
 }
