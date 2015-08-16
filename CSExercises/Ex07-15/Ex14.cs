a2b572f48359e12d1fbfefbdc3c23eebad577570
Summerwa <JWang004>
2015-08-16 19:24:22 +0800

    Completed Exercise 14-Juan

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            double a, b, c,s,area,A;
+            Console.Write("Input the side a:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input the side b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input the side c:");
+            c = Convert.ToDouble(Console.ReadLine());
+            s = (a + b + c) / 2;
+            A = s * (s - a) * (s - b) * (s - c);
+            area = Math.Sqrt(A);
+            Console.WriteLine("the area is: {0}", area);
+
         }
     }
 }
