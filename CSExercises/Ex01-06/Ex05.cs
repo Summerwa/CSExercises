f0e62665d05618aa77db809693120993a673c6a2
Summerwa <JWang004>
2015-08-16 15:25:10 +0800

    Completed Exercise 05-Juan

--- CSExercises/Ex01-06/Ex05.cs
+++ CSExercises/Ex01-06/Ex05.cs
@@ -10,7 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("input a double precision number:");
+            double In;
+            double Out;
+            In = Convert.ToDouble(Console.ReadLine());
+            Out = Math.Sqrt(In);
+            Console.WriteLine("square root={0}", Out);
+            Console.WriteLine("Rounded={0:0.000}", Out);
         }
     }
 }
