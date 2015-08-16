8b45f20ebcf0be2cbf0e9f44359cbecd6583c74a
Summerwa <JWang004>
2015-08-16 16:14:22 +0800

    Completed Exercise 09-Juan

--- CSExercises/Ex07-15/Ex09.cs
+++ CSExercises/Ex07-15/Ex09.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Input a number:");
+            double x;
+            double y;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = 2 * x * x - 4 * x + 3;
+            Console.WriteLine(y);
         }
     }
 }
