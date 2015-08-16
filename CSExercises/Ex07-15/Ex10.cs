98b90e3113af4c41e71c23cdb753c8c7d5cae6f7
Summerwa <JWang004>
2015-08-16 17:24:55 +0800

    Completed Exercise-10

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,7 +10,19 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double x1, y1, x2, y2; double square, distance;
+            Console.Write("Input x1:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input y1:");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input x2:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input y2:");
+            y2 = Convert.ToDouble(Console.ReadLine());
+            square = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+            distance=Math.Sqrt(square);
+            Console.WriteLine("the distance between these two points is: {0:0.###}",distance);
+        
         }
     }
 }
