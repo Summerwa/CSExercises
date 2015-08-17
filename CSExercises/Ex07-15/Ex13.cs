71ef07c28379231b67e5a3d3a87b15d640b63d6a
Summerwa <jwang39@163.com>
2015-08-17 11:47:08 +0800

    Completed Exercise

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -10,7 +10,21 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Input the distance:");
+            double D;
+            double F, F1;
+            D = Convert.ToDouble(Console.ReadLine());
+            F = 2.4 + 0.4 * D;
+            F1 = Math.Round(F, 1);
+            if (F1 > F)
+            {
+                Console.WriteLine("the total fare is ${0:0.00}", F1);
+            }
+            else
+            {
+                Console.WriteLine("the total fare is ${0:0.00}", F1 + 0.1); Console.WriteLine();
+            }
         }
     }
 }
+
--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -10,18 +10,22 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            double a, b, c,s,area,A;
+            double a, b, c, s, area, A;
             Console.Write("Input the side a:");
             a = Convert.ToDouble(Console.ReadLine());
             Console.Write("Input the side b:");
             b = Convert.ToDouble(Console.ReadLine());
             Console.Write("Input the side c:");
             c = Convert.ToDouble(Console.ReadLine());
-            s = (a + b + c) / 2;
-            A = s * (s - a) * (s - b) * (s - c);
-            area = Math.Sqrt(A);
-            Console.WriteLine("the area is: {0}", area);
-
+            if ((a + b > c) && (a + c > b) && (c + b > a))
+            {
+                s = (a + b + c) / 2;
+                A = s * (s - a) * (s - b) * (s - c);
+                area = Math.Sqrt(A);
+                Console.WriteLine("the area is: {0}", area);
+            }
+            else
+                Console.WriteLine("this is not a triangle");
         }
     }
 }
